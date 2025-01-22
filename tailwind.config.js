b5ee3c04389b46f395b88336dcf4aca096720926
윤효준 <yoonhyojun@yunhyojun-ui-MacBookAir.local>
2025-01-22 23:58:44 +0900

    chore: add custom colors and fonts to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnightBlack: '#02000E',
+        deepCharcoal: '#181D26',
+        steelGray: '#67666E',
+        neutralGray: '#828282',
+        slateBlue: '#8C92AB',
+        silverGray: '#A3A5A8',
+        softWhite: '#F7F7F8',
+        coralRed: '#F96D69',
+        pinkPunch: '#FE5493',
+      },
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'], // Pretendard 폰트 추가
+      },
+      letterSpacing: {
+        '5-percent': '0.05em', // Letter spacing 5%
+        '2-percent': '0.02em', // Letter spacing 2%
+        'minus-0.17px': '-0.17px', // Letter spacing -0.17px
+      },
+    },
   },
   plugins: [],
 };
