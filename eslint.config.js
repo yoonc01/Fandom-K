13f1dee03fa9be211719f46f1e7c68d7a2446b56
윤효준 <yoonhyojun@yunhyojun-ui-MacBookAir.local>
2025-01-22 22:22:45 +0900

    chore: add Prettier and configure auto-format on save

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    plugins: {
+      react,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "prettier/prettier": "error", // Prettier 규칙을 ESLint에서 오류로 표시
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  prettier, // Prettier 설정 추가
 ];
