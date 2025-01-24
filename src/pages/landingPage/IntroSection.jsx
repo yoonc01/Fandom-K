be70a1f4d05db16c91fc8819ab5276fad7dd3e76
Hyojun Yoon <hyoyoon@student.42seoul.kr>
2025-01-24 17:37:09 +0900

    feat: IntroSection done

---
+++ src/pages/landingPage/IntroSection.jsx
@@ -0,0 +1,34 @@
+import fandomKLogo from '@/assets/icons/FandomKLogo.svg';
+import idolImage from '@/assets/images/introSectionIdolImage.png';
+import leftTopGradient from '@/assets/images/leftTopGradient.png';
+
+function IntroSection() {
+  return (
+    <div className="relative flex flex-col items-center w-full h-screen bg-midnightBlack text-softWhite font-pretendard">
+      <img
+        src={leftTopGradient}
+        alt="leftTopGradient"
+        className="absolute top-[-52px] left-[-112px] opacity-40 pointer-events-none"
+      />
+      <div className="z-10 mt-[140px] text-center text-[26px] font-bold">
+        내가 좋아하는 아이돌을 <br />
+        가장 <span className="text-coralRed">쉽게 덕질</span> 하는 방법
+      </div>
+      <img
+        src={fandomKLogo}
+        alt="FandomKLogo"
+        className="z-10 mt-[20px] w-[236px] md:w-[324px] lg:w-[512px] select-none"
+      />
+      <img
+        src={idolImage}
+        alt="idolImage"
+        className="w-[392px] mt-[20px] h-auto opacity-70 top-[100px] md:w-[712px] lg:absolute lg:w-[932px] select-none"
+      />
+      <button className="z-10 mt-[40px] w-[230px] h-[48px] bg-gradient-to-r from-[#F86F65] to-pinkPunch rounded-[3px] text-[14px] z-10 md:mt-[100px] md:w-[477px] lg:mt-[584px]">
+        지금 시작하기
+      </button>
+    </div>
+  );
+}
+
+export default IntroSection;
