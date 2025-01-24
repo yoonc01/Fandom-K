f96eb27b75392645c93aaa318dff7d7bc65f235c
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-01-24 15:07:01 +0900

    feat: Add recharge credits function into 'CreditStorage'

--- src/utils/CreditStorage.js
+++ src/utils/CreditStorage.js
@@ -7,3 +7,10 @@ export const initCredits = () => {
 export const getCredits = () => {
   return parseInt(localStorage.getItem('credits'), 10) || 0;
 };
+
+export const rechgCredits = (amount) => {
+  if (amount > 0) {
+    const currentCredits = getCredits();
+    localStorage.setItem('credits', currentCredits + amount);
+  }
+};
