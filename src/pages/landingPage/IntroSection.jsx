011af179c693033b514921327489319201308faa
윤효준 <yoonhyojun@yunhyojun-ui-MacBookAir.local>
2025-01-25 15:20:06 +0900

    feat: add clickEvent on Button

--- src/pages/landingPage/IntroSection.jsx
+++ src/pages/landingPage/IntroSection.jsx
@@ -2,16 +2,25 @@ import fandomKLogo from '@/assets/icons/fandomKLogo.svg';
 import idolImage from '@/assets/images/introSectionIdolImage.png';
 import leftTopGradient from '@/assets/images/leftTopGradient.png';
 import PrimaryButton from '@/components/PrimaryButton';
+import { useNavigate } from 'react-router-dom';
+import { initCredits } from '@/utils/CreditStorage.js';
 
 function IntroSection() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    initCredits();
+    navigate('/list');
+  };
+
   return (
-    <div className="relative flex flex-col items-center w-full h-screen bg-midnightBlack text-softWhite font-pretendard">
+    <div className="py-[140px] relative flex flex-col items-center w-full bg-midnightBlack text-softWhite font-pretendard">
       <img
         src={leftTopGradient}
         alt="leftTopGradient"
         className="absolute top-[-52px] left-[-112px] opacity-40 pointer-events-none"
       />
-      <div className="z-10 mt-[140px] text-center text-[26px] font-bold">
+      <div className="z-10 text-center text-[26px] font-bold">
         내가 좋아하는 아이돌을 <br />
         가장 <span className="text-coralRed">쉽게 덕질</span> 하는 방법
       </div>
@@ -29,6 +38,7 @@ function IntroSection() {
         styles={
           'z-10 mt-[40px] w-[230px] h-[48px] rounded-[3px] text-[14px] z-10 md:mt-[100px] md:w-[477px] lg:mt-[584px]'
         }
+        onClickFunc={handleClick}
       >
         지금 시작하기
       </PrimaryButton>
