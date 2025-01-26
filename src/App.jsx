d7350cd24549c59f13756a039e87845c7760a839
juha399 <awe9224@naver.com>
2025-01-26 20:40:49 +0900

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '@/pages/landingPage/LandingPage';
 import ListPage from '@/pages/listPage/ListPage';
+import MyPage from '@/pages/myPage/MyPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="list" element={<ListPage />} />
+        <Route path="list" element={<ListPage />} /> 
+        <Route path="mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/myPage/MyPage.jsx
@@ -0,0 +1,16 @@
+import React from "react"; 
+
+
+
+const MyPage = () => {
+  return (
+ 
+<div>
+    <h1>My Page is working!!! <~/h1> 
+  </div>
+
+    
+  );
+};
+
+export default MyPage;
