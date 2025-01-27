07734f721d0c265afe2fba181c72068e4c5b176f
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-01-27 17:27:08 +0900

    feat: Locate the CreditSection onto the ListPage

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -1,8 +1,10 @@
 import DonationsList from '@/pages/listPage/DonationsList';
+import CreditSection from './CreditSection';
 
 function ListPage() {
   return (
-    <div class="bg-midnightBlack">
+    <div class="bg-midnightBlack flex flex-col items-center">
+      <CreditSection />
       <DonationsList />
     </div>
   );
