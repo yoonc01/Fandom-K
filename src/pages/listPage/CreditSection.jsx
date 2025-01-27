56fb2b544d299f186ec332ea3ab3c1e7da394bed
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-01-27 17:29:28 +0900

    feat: Make the CreditSection display the credits by getCredit()

--- src/pages/listPage/CreditSection.jsx
+++ src/pages/listPage/CreditSection.jsx
@@ -1,6 +1,8 @@
+import { getCredits } from '../../utils/CreditStorage';
+
 export default function CreditSection() {
   return (
-    <div className="h-[87px] pl-[70px] pr-[78px] border border-[#C1BECA] rounded-lg flex items-center justify-between w-full max-w-[1200px] min-w-[327px] pc:h-[131px] tablet:h-[131px]">
+    <div className="h-[87px] pl-[70px] pr-[78px] border border-[#C1BECA] rounded-lg flex items-center justify-between w-full max-w-[1200px] min-w-[327px] pc:h-[131px] tablet:h-[131px] mt-[16px] mb-[40px] tablet:mt-[0] tablet:mb-[64px] pc:my-[50px]">
       <div className="flex flex-col items-start">
         <p className="ml-[7px] font-pretendard font-normal text-[#9A999F] text-[12px] pc:text-[16px] tablet:text-[16px]">
           내 크레딧
@@ -8,7 +10,7 @@ export default function CreditSection() {
         <div className="flex items-center justify-between">
           <div className="w-[30px] h-[30px] bg-[url(@/assets/icons/credit.svg)] bg-contain bg-no-repeat bg-center" />
           <p className="font-pretendard font-bold text-[#DEDEE0] text-[20px] pc:text-[24px] tablet:text-[24px]">
-            36,000
+            {getCredits()}
           </p>
         </div>
       </div>
