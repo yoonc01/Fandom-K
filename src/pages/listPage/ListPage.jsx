7b76161587bb1289e8353f04b56bf4350ef4ca10
jihye5081 <ekftn90@gmail.com>
2025-01-27 18:53:40 +0900

    feat: Implement montlychart layout

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -1,9 +1,11 @@
 import DonationsList from '@/pages/listPage/DonationsList';
+import MonthlyChart from './monthlyChart';
 
 function ListPage() {
   return (
     <>
       <DonationsList />
+      <MonthlyChart />
     </>
   );
 }
