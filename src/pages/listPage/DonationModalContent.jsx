75d10b9887daf2ac03e747a54cfd53e7e26f595d
hyeonjiroh <sonamu6509@naver.com>
2025-01-28 16:33:02 +0900

    chore: Add mock data for layout

--- src/pages/listPage/DonationModalContent.jsx
+++ src/pages/listPage/DonationModalContent.jsx
@@ -1,3 +1,23 @@
+const mock = {
+  id: 572,
+  idolId: 1186,
+  title: '생일 광고',
+  subtitle: '강남역 광고',
+  targetDonation: 100000,
+  receivedDonations: 40000,
+  createdAt: '2024-09-25T12:08:47.530Z',
+  deadline: '2025-02-16T23:59:59.000Z',
+  status: false,
+  idol: {
+    id: 1186,
+    name: '윈터',
+    gender: 'female',
+    group: '에스파',
+    profilePicture: 'https://api.nudge-community.com/attachments/7033524',
+    totalVotes: 0,
+  },
+};
+
 function DonationModalContent() {
   return <div>Empty</div>;
 }
