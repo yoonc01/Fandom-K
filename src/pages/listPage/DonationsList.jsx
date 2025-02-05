e90f94f3e4895a83ad27d220b4e438d89253b5d2
hyeonjiroh <sonamu6509@naver.com>
2025-02-05 10:54:25 +0900

    refactor: Inprove DonationsList error handling

--- src/pages/listPage/DonationsList.jsx
+++ src/pages/listPage/DonationsList.jsx
@@ -9,29 +9,23 @@ function DonationsList({ onDonationClick }) {
   const [cursor, setCursor] = useState(0);
   const [cursorArr, setCursorArr] = useState([0]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [isPC, setIsPC] = useState(window.innerWidth >= 1200);
   const observerRef = useRef(null);
 
   const handleLoad = async (query) => {
     setIsLoading(true);
+    setIsError(false);
     try {
-      const { data, error } = await getItems(query);
-
-      if (error) {
-        console.error('Error loading items:', error);
-        alert('데이터를 불러오는 중 오류가 발생했습니다. 다시 시도해주세요.');
-        return;
-      }
-
-      if (!data) {
-        return;
-      }
-
+      const { data } = await getItems(query);
       const { list, nextCursor } = data;
 
       // PC가 아닌 경우, 무한 스크롤을 고려해서 새로 받은 데이터를 기존 데이터에 추가
       setItems((prev) => (isPC ? list : [...prev, ...list]));
       setCursor(nextCursor);
+    } catch (error) {
+      setIsError(true);
+      return;
     } finally {
       setIsLoading(false);
     }
@@ -81,9 +75,9 @@ function DonationsList({ onDonationClick }) {
   }, [cursor, isPC]);
 
   return (
-    <div className="flex flex-col gap-4 tablet:gap-6 pc:gap-8">
+    <div className="flex flex-col gap-4 tablet:gap-6 pc:gap-8 font-pretendard text-softWhite">
       <div className="w-full tablet:max-w-[1200px] mx-auto">
-        <h3 className="font-pretendard font-bold text-[16px] tablet:text-[20px] pc:text-[24px] text-softWhite">
+        <h3 className="font-bold text-[16px] tablet:text-[20px] pc:text-[24px]">
           후원을 기다리는 조공
         </h3>
       </div>
@@ -95,7 +89,7 @@ function DonationsList({ onDonationClick }) {
               <button
                 type="button"
                 onClick={handleLoadPrev}
-                disabled={cursorArr.length <= 1} // 이전 데이터가 없으면 이전 버튼 비활성화
+                disabled={cursorArr.length <= 1 || isError} // 이전 데이터가 없으면 이전 버튼 비활성화
                 className="bg-deepCharcoal opacity-80 text-white pt-[28.5px] pb-[30px] px-[15px] rounded-lg shrink-0 hover:opacity-70 disabled:opacity-50"
               >
                 <img src={prevIcon} alt="이전" />
@@ -103,7 +97,18 @@ function DonationsList({ onDonationClick }) {
             )}
 
             {/* 데이터 리스트 */}
-            <div className="w-full pc:max-w-[1200px]">
+            <div className="w-full pc:max-w-[1200px] ">
+              {isError ? (
+                <div className="w-full h-[300.5px] tablet:h-[407.5px] flex justify-center items-center bg-[#272F3D] rounded-lg">
+                  <div className="text-center font-normal text-[12px] tablet:text-[16px] pc:text-[20px]">
+                    데이터를 불러오는 중 오류가 발생했습니다.
+                    <br />
+                    새로고침 해주세요.
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
               {/* 로딩 중일 시 플레이스홀더 애니메이션 적용 */}
               {isLoading ? (
                 <div className="flex gap-2 tablet:gap-4 pc:gap-6">
@@ -138,7 +143,7 @@ function DonationsList({ onDonationClick }) {
               <button
                 type="button"
                 onClick={handleLoadNext}
-                disabled={cursor === null} // 다음 데이터가 없으면 다음 버튼 비활성화
+                disabled={cursor === null || isError} // 다음 데이터가 없으면 다음 버튼 비활성화
                 className="bg-deepCharcoal opacity-80 text-white pt-[28.5px] pb-[30px] px-[15px] rounded-lg shrink-0 hover:opacity-70 disabled:opacity-50"
               >
                 <img src={nextIcon} alt="다음" />
